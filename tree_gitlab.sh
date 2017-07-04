2dbf03cd8fc01a8d0adf74a928fe1d91e8a7af9f
chenliujin <liujin.chen@qq.com>
2017-07-04 17:06:23 +0800

    tree

---
+++ tree_gitlab.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+project=geomagnetism
+
+mkdir -p $project/01-document/00-原始需求文档和参考资料
+mkdir -p $project/01-document/01-需求分析
+mkdir -p $project/01-document/02-系统设计
+mkdir -p $project/01-document/03-详细设计
+mkdir -p $project/01-document/04-测试用例和测试数据
+mkdir -p $project/01-document/05-测试报告
+mkdir -p $project/01-document/06-项目总结报告
+mkdir -p $project/02-source/
+
+touch $project/01-document/00-原始需求文档和参考资料/README.md
+touch $project/01-document/01-需求分析/README.md
+touch $project/01-document/02-系统设计/README.md
+touch $project/01-document/03-详细设计/README.md
+touch $project/01-document/04-测试用例和测试数据/README.md
+touch $project/01-document/05-测试报告/README.md
+touch $project/01-document/06-项目总结报告/README.md
+touch $project/02-source/README.md
